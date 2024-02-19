f34824c9d7bac7e93b0fc1a15ff0e5bce166b5c1
tarun08pareta <tarun2001pareta@gmail.com>
2024-02-20 00:46:13 +0530

    learn how to install react and how to run react

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+  <h1>hello|| my name is Tarun Pareta and today learn how to install 
+    vite and react and how to run it
+  </h1>
+  )
+}
+
+export default App
